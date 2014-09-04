54251202eab77b4cbc17efc38cc7a714197f815a
Brig Lamoreaux <bklamoreaux@gmail.com>
2014-09-04 09:43:14 -0700

    Create join team Page. Add create team page. Add getMyTeams on Team Manager. Add routes for create new team. Use consistent button styles for Join Team.

--- src/RockIT/TechgamesBundle/Controller/GameController.php
+++ src/RockIT/TechgamesBundle/Controller/GameController.php
@@ -28,7 +28,15 @@ class GameController extends Controller
 
     public function joinAction($gameId)
     {
-        return $this->render('RockITTechgamesBundle:Game:join.html.twig', 
-            array('gameId' => $gameId));
+        $profileId = 1;
+
+        $gameManager = $this->get('gameManager');
+        $game = $gameManager->getGame($gameId);
+
+        $teamManager = $this->get('teamManager');
+        $myTeams  = $teamManager->getMyTeams($profileId);
+
+        return $this->render('RockITTechgamesBundle:Game:join.html.twig',
+            array('game' => $game, 'gameId' => $gameId, 'myTeams' => $myTeams));
     }
 }
