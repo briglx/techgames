c77103315c9c58a126b589912176b74b890e6e22
Brig Lamoreaux <bklamoreaux@gmail.com>
2014-09-03 20:30:09 -0700

    minor change

--- src/RockIT/TechgamesBundle/Model/ProfileManager.php
+++ src/RockIT/TechgamesBundle/Model/ProfileManager.php
@@ -12,7 +12,7 @@ class ProfileManager
         $this->_items[2] = new Profile(2, "Cat","Silverman", "csilverman@example.com");
         $this->_items[3] = new Profile(3, "Alfredo","Valdes", "avaldes@example.com");
         $this->_items[4] = new Profile(4, "Ken","Faulty", "kfault@example.com");
-        $this->_items[5] = new Profile(5, "Ken","Faulty", "kfault@example.com");
+        $this->_items[5] = new Profile(5, "Ty","Cobb", "tcobb@example.com");
         $this->_items[6] = new Profile(6, "Jack","Jackson", "jackson25@example.com");
 
     }
